0aee3933c60bd93d040d1cc4ad5ab048d1cd27ac
Shahroz Asif <shahrozasif@proton.me>
2025-04-12 15:27:09 +0500

    feat: add support for both png and jpg image files

--- src/main/java/com/bidsphere/bidsphere/controllers/Media.java
+++ src/main/java/com/bidsphere/bidsphere/controllers/Media.java
@@ -5,16 +5,18 @@ import com.bidsphere.bidsphere.services.ImageCleanup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.UUID;
 
 @RestController
@@ -30,23 +32,30 @@ public class Media {
         this.imageCleanup = imageCleanup;
     }
 
-    @PostMapping("/api/media/save")
-    public RESTResponse<String> saveMedia(@RequestParam("image") MultipartFile image) throws IOException {
-        if (image.isEmpty()) {
-            return RESTResponse.failed("Image is empty!");
-        }
+    public static class ImageRequest {
+        public String image;
+    }
 
+    @PostMapping("/media/save")
+    public RESTResponse<String> saveMedia(@RequestBody ImageRequest imageRequest) throws IOException {
         try {
             UUID imageUUID = UUID.randomUUID();
-            String filePath = UPLOAD_DIR + imageUUID.toString();
+            String filePath = UPLOAD_DIR + imageUUID.toString() + ".png";
+            byte[] decodedBytes = Base64.getDecoder().decode(imageRequest.image);
+            InputStream inputStream = new ByteArrayInputStream(decodedBytes);
+            BufferedImage image = ImageIO.read(inputStream);
+            if (image == null) {
+                return RESTResponse.failed("Unsupported image format or corrupted file");
+            }
 
-            Files.createDirectories(Paths.get(UPLOAD_DIR));
-            image.transferTo(new File(filePath));
+            File imageFile = new File(filePath);
+            ImageIO.write(image, "png", imageFile);
 
             imageCleanup.addImage(imageUUID.toString());
 
             return RESTResponse.passed(imageUUID.toString());
         } catch (IOException e) {
+            System.out.println(e.getMessage());
             return RESTResponse.failed("File upload failed!");
         }
     }
