ab3661ecc4a4668676d8072165a8097148f80b8b
Shahroz Asif <shahrozasif@proton.me>
2025-04-15 22:22:52 +0500

    fix: add missing errorcode file

---
+++ src/main/java/com/bidsphere/bidsphere/types/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.bidsphere.bidsphere.types;
+
+public enum ErrorCode {
+    NOT_ALLOWED,
+    INVALID_SESSION,
+    INVALID_CREDENTIALS,
+    INVALID_USER,
+    USER_EXISTS,
+    INVALID_IMAGE,
+    UPLOAD_FAILED,
+    LISTING_CREATION_FAILED,
+    INVALID_LISTING
+}
